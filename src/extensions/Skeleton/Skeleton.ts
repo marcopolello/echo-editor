3b86056a2b1c1065c2a9685bce1a41bb32aa3dc2
marcopolello <marco.polello@enerp.it>
2024-06-07 16:07:22 +0200

    menu per skeleton pre impostati

--- src/extensions/Skeleton/Skeleton.ts
+++ src/extensions/Skeleton/Skeleton.ts
@@ -6,7 +6,11 @@ import { DEFAULT_SKELETONS_LIST } from '@/constants'
 
 // src/extensions/Skeleton/Skeleton.ts
 
-let skeletons = [1, 2, 3];
+let skeletons = [
+  {id: "1", title: "skeleton1", content: "content1"}, 
+  {id: "2", title: "skeleton2", content: "content2"}, 
+  {id: "3", title: "skeleton3", content: "content3"}
+];
 
 export const Skeleton = Extension.create({
   name: 'skeleton',
@@ -17,10 +21,10 @@ export const Skeleton = Extension.create({
       button({ editor, extension, t }) {
         const skeletons = extension.options?.skeletons || [];
         const items: Item[] = [...skeletons].map(k => ({
-          title: "skeletons",
+          title: k.title,
           isActive: () => true,
           action: () => {
-            insertCustomHTML(editor, String(k));
+            insertCustomHTML(editor, String(k.title));
           },
           disabled: false,
           divider: false,
@@ -43,7 +47,9 @@ export const Skeleton = Extension.create({
 });
 
 // Definisci la funzione che inserirà l'HTML personalizato
-function insertCustomHTML(editor, skeletonId) {
-  const customHTML = '<div class="custom-html">Custom HTML Content for Skeleton ' + skeletonId + '</div>';
+function insertCustomHTML(editor, skeletonTitle) {
+  //CUSTOM HTML PRESO DA DB O CHIAMATA AXIOS
+  //TODO
+  const customHTML = '<div class="custom-html">Custom HTML Content for Skeleton: ' + skeletonTitle + '</div>';
   editor.chain().focus().insertContent(customHTML).run();
 }
--- src/extensions/Skeleton/components/SkeletonActionButton.vue
+++ src/extensions/Skeleton/components/SkeletonActionButton.vue
@@ -24,13 +24,15 @@ const props = defineProps<{ items: Item[]; editor: any; tooltip: string }>();
 
 const { t } = useLocale();
 
+const skeletons = "skeletons";
+
 const active = computed(() => {
   const find: any = props.items.find((k: any) => k.isActive());
   if (find) {
     return find;
   }
   const item: Item = {
-    title: "SKELETON",
+    title: "SKELETON ESEMPIO",
     isActive: () => false,
   };
   return item;
@@ -40,11 +42,11 @@ const active = computed(() => {
 <template>
   <DropdownMenu>
     <DropdownMenuTrigger>
-      <ActionMenuButton :title="active.title" :tooltip="props.tooltip" />
+      <ActionMenuButton :title="skeletons" :tooltip="skeletons" />
     </DropdownMenuTrigger>
     <DropdownMenuContent class="w-32 overflow-y-auto max-h-96">
       <template v-for="(item, index) in props.items" :key="index">
-        <DropdownMenuCheckboxItem :checked="active.title === item.title" @click="item.action">
+        <DropdownMenuCheckboxItem @click="item.action">
           <div class="h-full ml-1">{{ item.title }}</div>
         </DropdownMenuCheckboxItem>
       </template>
